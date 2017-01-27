2fffecbdcd61a33a5c0e213e284b9086e4b14dbb
jlyee <justinmlye@gmail.com>
2017-01-27 16:56:41 -0600

    simple shape working.

--- projects/opengl/main.cpp
+++ projects/opengl/main.cpp
@@ -1,15 +1,47 @@
 #include<iostream>
+#define GLEW_STATIC
+#include<GL/glew.h>
+#include<GLFW/glfw3.h>
 #include"jlg_window.h"
-
-
+#include"shape.h"
+#include"Shader.h"
 int main(int argc, char* argv[]) {
-	jlg::AppWindow win;
-	glfwMakeContextCurrent(win.Handle());
+    jlg::Window win(800,600,"Window");
+	win.SetViewport();
+
+	jlg::Shader shader("core.vs", "core.frag");
+	
+	GLfloat verts[] = {
+		0.5f, 0.5f, 0.0f,
+		0.0f, 0.0f, 0.0f,
+		-0.5f, 0.5f, 0.0f
+	};
+	jlg::shape triangle(verts, 3, 3);
+	GLuint VAO, VBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	triangle.add_attrib(3);
+	triangle.buffer(VAO, VBO);
+
+	//glBindVertexArray(VAO);
+	//glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	//glBufferData(GL_ARRAY_BUFFER, triangle.vertex_size(), triangle.vertices(), GL_STATIC_DRAW);
+	//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	//glEnableVertexAttribArray(0);
+	//glBindVertexArray(0);
+
 	while (!glfwWindowShouldClose(win.Handle())) {
 		glfwPollEvents();
-		win.ClearColorBuffer();
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		shader.UseProgram();
+		glBindVertexArray(VAO);
+		triangle.draw();
+		glBindVertexArray(0);
 		glfwSwapBuffers(win.Handle());
 	}
+	glDeleteVertexArrays(1,&VAO);
+	glDeleteBuffers(1,&VBO);
 	glfwTerminate();
 	return EXIT_SUCCESS;
 }
--- src/shape.cpp
+++ src/shape.cpp
@@ -11,6 +11,15 @@ jlg::vertex_data::vertex_data(GLfloat* data, const GLuint& Dimensions, const GLu
 		_data[i] = data[i];
 }
 
+jlg::vertex_data::vertex_data(const vertex_data& other) :
+	_data(nullptr),
+	_dims(other._dims),
+	_count(other._count) {
+	_data = new GLfloat[_dims * _count];
+	for(GLuint i = 0; i < (_dims * _count); i++)
+		_data[i] = other._data[i];
+}
+
 jlg::vertex_data::~vertex_data() {
 	if (_data != nullptr) {
 		GLfloat* del_buffer = _data;
@@ -20,4 +29,38 @@ jlg::vertex_data::~vertex_data() {
 }
 
 jlg::shape::shape(GLfloat* VertexData, const GLuint& Dimensions, const GLuint& VertexCount) :
-	vertex_data(VertexData, Dimensions, VertexCount) {}
+	vertex_data(VertexData, Dimensions, VertexCount),
+	_next_index(0),
+	_total_count(0) {}
+
+jlg::shape::shape(const vertex_data& copy_data) :
+	vertex_data(copy_data),
+	_next_index(0),
+	_total_count(0) {}
+
+void jlg::shape::add_attrib(const GLuint& Count) {
+	attribs.push_back(attribute(_next_index, Count, vertex_dims(), _total_count));
+	_next_index++;
+	_total_count += Count;
+}
+
+void jlg::shape::buffer(const GLuint& generated_vao, const GLuint& generated_vbo) {
+	glBindVertexArray(generated_vao);
+	glBindBuffer(GL_ARRAY_BUFFER, generated_vbo);
+	glBufferData(GL_ARRAY_BUFFER, this->vertex_size(), this->vertices(), GL_STATIC_DRAW);
+	for (GLuint i = 0; i < attribs.size(); i++) {
+		glVertexAttribPointer(
+			attribs[i].index,
+			attribs[i].count,
+			GL_FLOAT,
+			GL_FALSE,
+			this->vertex_dims() * sizeof(GLfloat),
+			(GLvoid*)(attribs[i].offset * sizeof(GLfloat)));
+		glEnableVertexAttribArray(attribs[i].index);
+	}
+	glBindVertexArray(generated_vao);
+}
+
+void jlg::shape::draw() const {
+	glDrawArrays(GL_TRIANGLES, 0, (GLsizei)this->vertex_count());
+}
